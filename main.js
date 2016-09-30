197c82ea9361876c3b19dae76430ba94b88ffd46
sartechb <sarthak1657@gmail.com>
2016-09-30 11:02:30 -0400

    new home page done, calendar in the works

--- main.js
+++ main.js
@@ -3,6 +3,12 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('#calendar').fullCalendar({
+        googleCalendarApiKey: 'AIzaSyAMdAKe1QdTiRUpW8oLZAfkLTGzz3NhUfM',
+        events: {
+            googleCalendarId: 'csescholarsum@gmail.com'
+        }
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
