373ed3cffd36bbf4d4a59f7997b205c85313dd81
sartechb <sarthak1657@gmail.com>
2016-09-30 11:47:00 -0400

    forget it just going with default widget

--- main.js
+++ main.js
@@ -3,12 +3,5 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('#calendar').fullCalendar({
-        googleCalendarApiKey: 'AIzaSyAMdAKe1QdTiRUpW8oLZAfkLTGzz3NhUfM',
-        events: {
-            googleCalendarId: 'csescholarsum@gmail.com'
-        }
-    });
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
