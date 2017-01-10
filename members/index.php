9f454f69db9acd85cca9e36ed1a18c3859b60c2c
mavcook <mavcook@umich.edu>
2017-01-09 23:01:22 -0500

    members css and compliments

--- members/index.php
+++ members/index.php
@@ -1,6 +1,57 @@
 
 
   <?php 
+		$compliments = array("You look wonderful today", "You're cool",
+"Your smile is contagious",
+"You look great today",
+"You're a smart cookie",
+"I like your style",
+"You have the best laugh",
+"I appreciate you",
+"Your perspective is refreshing",
+"You're an awesome friend",
+"You light up the room",
+"You deserve a hug right now",
+"You should be proud of yourself",
+"You have a great sense of humor",
+"You've got all the right moves",
+"On a scale from 1 to 10, you're an 11",
+"You are brave",
+"Your eyes are breathtaking",
+"You are making a difference",
+"You're like sunshine on a rainy day",
+"You bring out the best in other people",
+"You're a great listener",
+"I bet you sweat glitter",
+"That color is perfect on you",
+"Hanging out with you is always a blast",
+"Being around you makes everything better",
+"You're wonderful",
+"Jokes are funnier when you tell them",
+"Your hair looks stunning",
+"You're one of a kind",
+"You're inspiring",
+"Our community is better because you're in it",
+"You have the best ideas",
+"You're a great example to others",
+"You could survive a Zombie apocalypse",
+"You're more fun than bubble wrap",
+"Any team would be lucky to have you on it",
+"I bet you do the crossword puzzle in ink",
+"Babies and small animals probably love you",
+"You're someone's reason to smile",
+"You have a good head on your shoulders.",
+"You're really something special",
+"You're a gift to those around you"
+);
+			
+		function randomCompliment()
+		{
+			global $compliments;
+			$output = array_rand($compliments, 1);
+			echo $compliments;
+		}
+		
 		define("CURRENT_PAGE", "Members");
 		define("JUMP", "../"); #hacky
 		include '../includes/header.php';
@@ -28,8 +79,9 @@
 	<div class="fill">
 		
 		<div id="status" class="right-align">
-				Hello <?php echo $_SERVER['REMOTE_USER']; ?><br>
-				You have earned <b><?php echo $points; ?></b> points.
+			<h5>Hello <?php echo $_SERVER['REMOTE_USER']; ?></h5>
+			<?php randomCompliment(); ?> and<br>
+			You have earned <b><?php echo $points; ?></b> points.
 		</div>
 
 		<?php 
