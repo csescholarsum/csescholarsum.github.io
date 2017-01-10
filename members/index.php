1274782ed13aacc3045861ebdab9f66d620cc1aa
mavcook <mavcook@umich.edu>
2017-01-09 22:36:04 -0500

    push footer to bottom on members page

--- members/index.php
+++ members/index.php
@@ -25,36 +25,37 @@
 		}
 	
 	?>
+	<div class="fill">
+		
+		<div id="status" class="right-align">
+				Hello <?php echo $_SERVER['REMOTE_USER']; ?><br>
+				You have earned <b><?php echo $points; ?></b> points.
+		</div>
 
-	<div id="status" class="right-align">
-			Hello <?php echo $_SERVER['REMOTE_USER']; ?><br>
-			You have earned <b><?php echo $points; ?></b> points.
-	</div>
-	
-	<?php 
-		$events = getCurrentEvents();
-		foreach ($events as $ev) { 
-	?> <!-- display all open events -->
-		<div class="wrap-event">
-			<h1 class="header center blue-text"><?php echo $ev['name']; ?></h1>
-			<div class="row">
-				<div class="col s12">
-					<div class="input-field col s12">
-						<form name="" method="post">
-							<input type="hidden" name="eventid" value="<?php echo $ev['eventid']?>">
-							<label for="eventCode">Event Code</label>
-							<input type="text" name="eventCode" id="eventCode">
-							<button class="btn waves-effect waves-light blue" type="submit">Submit
-							<i class="material-icons right">send</i>
-							</button>
-						</form>
+		<?php 
+			$events = getCurrentEvents();
+			foreach ($events as $ev) { 
+		?> <!-- display all open events -->
+			<div class="wrap-event">
+				<h1 class="header center blue-text"><?php echo $ev['name']; ?></h1>
+				<div class="row">
+					<div class="col s12">
+						<div class="input-field col s12">
+							<form name="" method="post">
+								<input type="hidden" name="eventid" value="<?php echo $ev['eventid']?>">
+								<label for="eventCode">Event Code</label>
+								<input type="text" name="eventCode" id="eventCode">
+								<button class="btn waves-effect waves-light blue" type="submit">Submit
+								<i class="material-icons right">send</i>
+								</button>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	<?php } ?> <!-- end loop displaying events -->
-	
-	
+		<?php } ?> <!-- end loop displaying events -->
+
+	</div> <!-- end .fill -->	
   
   <?php include '../includes/footer.php' ?>
 
